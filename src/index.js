0ebc4aa81eb8c08756ab8375a3d57fad4063c9ef
marmooo <marmooo@users.noreply.github.com>
2023-04-26 00:02:22 +0900

    improve loading time

--- src/index.js
+++ src/index.js
@@ -634,6 +634,7 @@ class SoundFontPlayer {
 
   async loadSoundFontBuffer(soundFontBuffer) {
     if (!this.synth) {
+      await JSSynthPromise;
       await this.context.audioWorklet.addModule(
         "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/externals/libfluidsynth-2.3.0-with-libsndfile.min.js",
       );
@@ -1350,6 +1351,20 @@ function initQuery() {
   return query;
 }
 
+function loadLibraries(urls) {
+  const promises = urls.map((url) => {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = url;
+      script.async = true;
+      script.onload = resolve;
+      script.onerror = reject;
+      document.body.appendChild(script);
+    });
+  });
+  return Promise.all(promises);
+}
+
 const noteHeight = 30;
 let controllerDisabled;
 let keyEvents = [];
@@ -1379,6 +1394,12 @@ const scoreModal = new bootstrap.Modal("#scorePanel", {
   backdrop: "static",
   keyboard: false,
 });
+
+Module = {};
+const JSSynthPromise = loadLibraries([
+  "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
+  "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/externals/libfluidsynth-2.3.0-with-libsndfile.min.js",
+]);
 
 document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
 document.getElementById("toggleColor").onclick = toggleRectColor;
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-04-16 16:50";
+var CACHE_NAME = "2023-04-26 00:00";
 var urlsToCache = [
   "/tip-tap-notes/",
   "/tip-tap-notes/index.js",
@@ -6,7 +6,10 @@ var urlsToCache = [
   "/tip-tap-notes/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
-  "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js,npm/js-synthesizer@1.8.5/externals/libfluidsynth-2.3.0-with-libsndfile.min.js,npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
+  "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js",
+  "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
+  "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.worklet.min.js",
+  "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/externals/libfluidsynth-2.3.0-with-libsndfile.min.js",
 ];
 
 self.addEventListener("install", function (event) {
