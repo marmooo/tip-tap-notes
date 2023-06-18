51deda223f13739430bbe73beb37e5c999f05ea1
marmooo <marmooo@users.noreply.github.com>
2023-06-18 13:38:21 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,9 @@
-var CACHE_NAME = "2023-05-07 00:30";
+var CACHE_NAME = "2023-06-18 13:40";
 var urlsToCache = [
   "/tip-tap-notes/",
   "/tip-tap-notes/index.js",
   "/tip-tap-notes/abt.mid",
   "/tip-tap-notes/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
