bd7df1ffb8cb6330bc9044f516caa009f33d5f61
marmooo <marmooo@users.noreply.github.com>
2025-09-09 00:38:16 +0900

    bump bootstrap from 5.3.7 to 5.3.8

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "2025-06-22 03:20";
+const CACHE_NAME = "2025-09-09 00:19";
 const urlsToCache = [
   "/tip-tap-notes/",
   "/tip-tap-notes/index.js",
   "/tip-tap-notes/abt.mid",
   "/tip-tap-notes/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.worklet.min.js",
