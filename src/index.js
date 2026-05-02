a1693eb50f866cd9b5a02b5d71a2dc8a8ab3a17c
marmooo <marmooo@users.noreply.github.com>
2026-05-02 11:21:41 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-function loadConfig() {
-  if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
-}
-
 function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") == 1) {
-    localStorage.setItem("darkMode", 0);
-    document.documentElement.setAttribute("data-bs-theme", "light");
-  } else {
-    localStorage.setItem("darkMode", 1);
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
+  const html = document.documentElement;
+  const newTheme = html.getAttribute("data-bs-theme") === "dark"
+    ? "light"
+    : "dark";
+  html.setAttribute("data-bs-theme", newTheme);
+  localStorage.setItem("darkMode", newTheme);
 }
 
 function getRandomInt(min, max) {
@@ -1385,7 +1378,7 @@ let tapCount = 0;
 let perfectCount = 0;
 let greatCount = 0;
 let firstRun = true;
-loadConfig();
+
 if (location.search) {
   loadMIDIFromUrlParams();
 } else {
