c52d3c2e64f2c18d498a7328c89e523dae543dff
marmooo <marmooo@users.noreply.github.com>
2026-09-26 22:31:38 +0900

    prerollSec = 20

--- src/index.js
+++ src/index.js
@@ -1837,6 +1837,7 @@ function switchMode(next) {
 
 const audioContext = new AudioContext();
 const midy = new Midy(audioContext);
+midy.prerollSec = 20;
 midy.cacheMode = CACHE_MODES.includes(config.cacheMode)
   ? config.cacheMode
   : "chunk";
